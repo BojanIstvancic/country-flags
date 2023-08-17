71be549429f12021e1853c21147577435bbf87f0
Bojan <bojan.istvancic@gmail.com>
2023-08-17 11:03:41 +0200

    create basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Home";
+import Details from "./Details";
 
 function App() {
   return (
-    <div className="App">
-      <p>Our app</p>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/details"}>Details</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Details.tsx
@@ -0,0 +1,9 @@
+function Details() {
+  return (
+    <div className="Details">
+      <p>Details Page</p>
+    </div>
+  );
+}
+
+export default Details;
