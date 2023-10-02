ffcfc42e6c9848634bc8e4a8bafd10b8baa846f2
Bojan <bojan.istvancic@gmail.com>
2023-10-02 16:24:16 +0200

    add dispatch in App, pull the number for comparison

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { useRoutes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import { useAppDispatch } from "./redux/hooks";
 import { getAddedPokemons } from "./redux/features/comparison/comparisonSlice";
+import { useEffect } from "react";
 
 const App = () => {
   const appRoutes = useRoutes(routes);
   const dispatch = useAppDispatch();
 
-  dispatch(getAddedPokemons());
+  useEffect(() => {
+    dispatch(getAddedPokemons());
+  }, [dispatch]);
 
   return <Layout>{appRoutes}</Layout>;
 };
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -8,11 +8,13 @@ import {
 } from "../../redux/features/theme/themeSlice";
 import { Theme } from "../../shared/types";
 import { Link } from "react-router-dom";
-import { memo } from "react";
+import CompareArrowsIcon from "@mui/icons-material/CompareArrows";
+import { selectAmountOfPokemons } from "../../redux/features/comparison/comparisonSlice";
 
 const Header = () => {
   const dispatch = useAppDispatch();
   const { theme } = useAppSelector(selectTheme);
+  const amountOfComparisonPokemons = useAppSelector(selectAmountOfPokemons);
 
   const handleChangeTheme = () => {
     dispatch(changeTheme());
@@ -20,7 +22,9 @@ const Header = () => {
 
   return (
     <div
-      className={`${theme === Theme.DARK ? "bg-slate-700" : "bg-slate-300"}`}
+      className={`${
+        theme === Theme.DARK ? "bg-slate-700" : "bg-slate-300"
+      } sticky top-0 z-50`}
     >
       <div
         className={`container mx-auto p-4 flex items-center justify-between`}
@@ -28,13 +32,21 @@ const Header = () => {
         <h6 className="mb-0 cursor-pointer">
           <Link to="/">Pokemons</Link>
         </h6>
-        <div className="cursor-pointer" onClick={handleChangeTheme}>
-          {theme === Theme.DARK && <NightlightRoundedIcon />}
-          {theme === Theme.LIGHT && <WbSunnyRoundedIcon />}
+        <div className="flex">
+          <div className="cursor-pointer mr-2 relative">
+            <CompareArrowsIcon />
+            <div className="absolute right-0 bottom-0 bg-slate-900">
+              {amountOfComparisonPokemons}
+            </div>
+          </div>
+          <div className="cursor-pointer" onClick={handleChangeTheme}>
+            {theme === Theme.DARK && <NightlightRoundedIcon />}
+            {theme === Theme.LIGHT && <WbSunnyRoundedIcon />}
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default memo(Header);
+export default Header;
